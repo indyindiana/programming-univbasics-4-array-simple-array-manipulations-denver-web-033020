052716f914ed145fac3e118dc45e9d5f619b8a9a
indiana danaher <indiana.danaher@gmail.com>
2020-03-10 06:28:04 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -42,8 +42,8 @@
       array.delete(string)
     end
     
-    def using_delete_at(array, element)
-      array.delete(element)
+    def using_delete_at(array, index)
+      array.delete(index)
     end
     
     
